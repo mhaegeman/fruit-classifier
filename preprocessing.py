4104be98e3bbea8c383fef78d7043f44033d6a4c
Maxime Haegeman <74610219+mhaegeman@users.noreply.github.com>
2023-04-07 10:28:25 +0200

    Adding PCA image and process in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras import Model
 from pyspark.sql.functions import col, pandas_udf, PandasUDFType, element_at, split, udf
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import PCA
+from pyspark.ml.feature import PCA, PCAModel
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.ml.feature import VectorAssembler
 import matplotlib.pyplot as plt
@@ -112,64 +112,46 @@ def featurize_udf(content_series_iter):
 # If images are too big avoid OOM with:
 # spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "1024")
 
+#################################### PCA STUDY ########################################
+# explained_variances=[]
+
+# num_components = features_vec_df.select("features_vec").first()[0].size
+
+
+# # Iterate through each component to calculate the explained variance
+# for k in range(1, 11):
+#     pca = PCA(k=k, inputCol="features_vec", outputCol="pca_features")
+#     pca_model = pca.fit(features_vec_df)
+#     explained_variances.append(sum(pca_model.explainedVariance))
+    
+
+# # Plot the elbow curve analysis
+# plt.plot(range(1, k + 1), explained_variances, marker='o')
+# plt.xlabel('Number of Principal Components')
+# plt.ylabel('Cumulative Explained Variance')
+# plt.title('Elbow Curve Analysis')
+# plt.savefig('Elbow_Curve_Analysis.png')
+# plt.show()
+
 def perform_pca(features_df):
-    """
-    This function performs Principal Component Analysis (PCA) on the input features DataFrame.
-    It calculates the number of principal components required to capture at least 80% of the global variation.
-    :param features_df: A DataFrame containing the features extracted from the images.
-    :return: A DataFrame with PCA features, retaining at least 80% of the global variation.
-    """
 
-    # Convert the array column to a dense vector column
-    to_vector = udf(lambda x: Vectors.dense(x), VectorUDT())
-    features_vec_df = features_df.withColumn("features_vec", to_vector("features"))
+  to_vector = udf(lambda x: Vectors.dense(x), VectorUDT())
+  features_vec_df = features_df.withColumn("features_vec", to_vector("features"))
+  
+  pca = PCA(k=8,
+          inputCol="features_vec",
+          outputCol="pca_features")
 
+  pca_model = pca.fit(features_vec_df)
 
-    # Initialize a list to store explained variances
-    explained_variances = []
-    
-    # Get the number of components in the feature vector
-    num_components = features_vec_df.select("features_vec").first()[0].size
-
-    # Iterate through each component to calculate the explained variance
-    for k in range(1, num_components + 1):
-        pca = PCA(k=k, inputCol="features_vec", outputCol="pca_features")
-        pca_model = pca.fit(features_vec_df)
-        explained_variances.append(sum(pca_model.explainedVariance))
-        
-        # Stop when the cumulative explained variance is greater than or equal to 80%
-        if explained_variances[-1] >= 0.8:
-            break
-
-    # Plot the elbow curve analysis
-    plt.plot(range(1, k + 1), explained_variances, marker='o')
-    plt.xlabel('Number of Principal Components')
-    plt.ylabel('Cumulative Explained Variance')
-    plt.title('Elbow Curve Analysis')
-    plt.savefig('Elbow_Curve_Analysis.png')
-
-    # Write the elbow curve analysis report to a text file
-    with open('Elbow_Curve_Analysis_Report.txt', 'w') as f:
-        f.write('Elbow Curve Analysis Report\n')
-        f.write('Number of Principal Components: {}\n'.format(k))
-        f.write('Cumulative Explained Variance: {}\n'.format(explained_variances[-1]))
-
-    # Apply PCA transformation to the features DataFrame and select the necessary columns
-    pca_df = pca_model.transform(features_vec_df).select("path", "label", "pca_features")
-    
-    # Save the elbow curve analysis report and plot to the specified S3 folder
-    sc.parallelize([('Elbow_Curve_Analysis.png', 'Elbow_Curve_Analysis.png')]) \
-        .saveAsTextFile(PATH_Report)
-
-    return pca_df
+  res = pca_model.transform(features_vec_df)
+  return res
 
 # Apply functions
-features_df = images.repartition(20).select(col("path"),
+features_pca_df = perform_pca(images.repartition(20).select(col("path"),
                                             col("label"),
                                             featurize_udf("content").alias("features")
-                                           )
-
-pca_df = perform_pca(features_df)
+                                           ))
 
 # Saving pca_df in Parquet format
-pca_df.write.mode("overwrite").csv(PATH_Result)
+features_pca_df.write.mode("overwrite").parquet(PATH_Result)
